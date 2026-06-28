3ee98f9295cf90fa8c1c0cf4c50e82eba8d8c3a4
Ethan <ethanhowes06@gmail.com>
2026-06-28 17:32:07 -0400

    feat: created terraform module from existing template for docker compose

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ module "k3s_worker_01" {
   ip             = "192.168.0.121/24"
   ssh_public_key = var.ssh_public_key
 }
+
+module "docker_compose_01" {
+  source         = "./modules/docker-compose"
+  vm_id          = 301
+  name           = "docker-compose-01"
+  ip             = "192.168.0.131/24"
+  ssh_public_key = var.ssh_public_key
+}
---
+++ terraform/modules/docker-compose/main.tf
@@ -0,0 +1,61 @@
+# All the standard info needed to initialize a VM from template
+
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "this" {
+  vm_id     = var.vm_id
+  name      = var.name
+  node_name = "proxmox-datacenter"
+
+  clone {
+    # tmpl-docker-compose
+    vm_id = 101
+    full  = true
+  }
+
+cpu {
+  cores = var.cores
+  type  = "x86-64-v2"
+}
+
+  memory {
+    dedicated = var.memory
+  }
+
+  disk {
+    datastore_id = var.storage
+    interface    = "scsi0"
+    discard      = "on"
+    size         = 20
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    datastore_id = var.cloudinit_storage
+    ip_config {
+      ipv4 {
+        address = var.ip
+        gateway = var.gateway
+      }
+    }
+    user_account {
+      username = "ubuntu"
+      keys     = [var.ssh_public_key]
+    }
+  }
+
+  operating_system {
+    # Linux 2.6+ kernel
+    type = "l26"
+  }
+}
+
