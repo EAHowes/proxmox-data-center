9cf6f5260aa80abfb8af34ca64c2ec3530364415
Ethan <ethanhowes06@gmail.com>
2026-06-28 16:51:09 -0400

    feat: added a terraform module to for a k3s worker. Builds off previously created template

---
+++ terraform/modules/k3s-node/main.tf
@@ -0,0 +1,51 @@
+# All the standard info needed to initialize a VM from template
+resource "proxmox_virtual_environment_vm" "this" {
+  vm_id     = var.vm_id
+  name      = var.name
+  node_name = "proxmox-datacenter"
+
+  clone {
+    # tmpl-k3s-worker
+    vm_id = 100
+    full  = true
+  }
+
+  cpu {
+    cores = var.cores
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = var.memory
+  }
+
+  disk {
+    datastore_id = var.storage
+    interface    = "scsi0"
+    discard      = "on"
+    size         = 20
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.ip
+        gateway = var.gateway
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = [var.ssh_public_key]
+    }
+  }
+
+  operating_system {
+    # Linux 2.6+ kernel
+    type = "l26"
+  }
+}
