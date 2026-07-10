b6c54295848c2da9b13afa04c8a777cc6dd200ab
Ethan <ethanhowes06@gmail.com>
2026-07-10 01:32:48 -0400

    feat: added three new worker nodes to the cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,30 @@ module "k3s_worker_01" {
   ssh_public_key = var.ssh_public_key
 }
 
+module "k3s_worker_02" {
+  source         = "./modules/k3s-node"
+  vm_id          = 202
+  name           = "k3s-worker-02"
+  ip             = "192.168.0.122/24"
+  ssh_public_key = var.ssh_public_key
+}
+
+module "k3s_worker_03" {
+  source         = "./modules/k3s-node"
+  vm_id          = 203
+  name           = "k3s-worker-03"
+  ip             = "192.168.0.123/24"
+  ssh_public_key = var.ssh_public_key
+}
+
+module "k3s_worker_04" {
+  source         = "./modules/k3s-node"
+  vm_id          = 204
+  name           = "k3s-worker-04"
+  ip             = "192.168.0.124/24"
+  ssh_public_key = var.ssh_public_key
+}
+
 module "docker_compose_01" {
   source         = "./modules/docker-compose"
   vm_id          = 301
